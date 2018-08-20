39fb6d30b92132faa8765ab17407de7d97b5d298
Matthieu H <mat.houdebine@wanadoo.fr>
2018-08-20 21:16:22 +0200

    Get PNG from this repository to get the most completed set

--- replace-emojis.js
+++ replace-emojis.js
@@ -3,7 +3,7 @@ var pattern = "*://ssl.gstatic.com/dynamite/emoji/*";
 function redirect(requestDetails) {
     // Replace origin URL with archived Google Emoji equivalent from noto-emoji repository
     return {
-        redirectUrl: requestDetails.url.replace("ssl.gstatic.com/dynamite/emoji", "raw.githubusercontent.com/googlei18n/noto-emoji/914c9ecb9d5e6d96c972c8df577cc6a36d162ece")
+        redirectUrl: requestDetails.url.replace("ssl.gstatic.com/dynamite/emoji/png/32", "raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis")
     };
 }
 
