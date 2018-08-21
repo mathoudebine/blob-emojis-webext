ef1d899f0187e50ef1034af354c4d19efac23e6d
mathoudebine <mat.houdebine@wanadoo.fr>
2018-08-21 14:07:09 +0200

    Add comments, update README

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ To say no to conformism ! Who wants to have the same boring round-shaped emojis 
 This extension includes:
 
 * a content script, "replace-emojis.js", that is injected into any pages under "messages.android.com" or any of its subdomains
-* potato emojis from Android 6.0.1 from https://github.com/googlei18n/noto-emoji/commit/914c9ecb9d5e6d96c972c8df577cc6a36d162ece, completed with newer versions for missing emojis (7.0, 8.0, 9.0)
+* blob emojis from Android 6.0.1 from https://github.com/googlei18n/noto-emoji/commit/914c9ecb9d5e6d96c972c8df577cc6a36d162ece, completed with newer versions for missing emojis (7.0, 8.0, 9.0)
 
-The content script intercepts HTTP requests to the CDN hosting latest Noto emojis (ssl.gstatic.com) and redirects these requests to an archived version of the noto-emoji on this Github repository. This archived version is composed of all Android 6.0.1 emojis, completed with emojis from Android 7.0, then from Android 8.0, for the missing ones only.
+The content script intercepts HTTP requests to the Google CDN hosting latest Noto emojis (ssl.gstatic.com) and redirects these requests to an archived version of the noto-emoji from this Github repository.
 
--- replace-emojis.js
+++ replace-emojis.js
@@ -1,14 +1,20 @@
+// Pattern of emojis loaded from the Google CDN
 var pattern = "*://ssl.gstatic.com/dynamite/emoji/*";
 
+// Callback to replace origin URL with archived Google Emoji equivalent from noto-emoji repository
 function redirect(requestDetails) {
-    // Replace origin URL with archived Google Emoji equivalent from noto-emoji repository
-    newUrl = requestDetails.url.replace("ssl.gstatic.com/dynamite/emoji/png/32","raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis");
-    newUrl = newUrl.replace("ssl.gstatic.com/dynamite/emoji/png/128", "raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis");
+    
+    newUrl = requestDetails.url.replace("ssl.gstatic.com/dynamite/emoji/png/32",
+        "raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis");
+    newUrl = newUrl.replace("ssl.gstatic.com/dynamite/emoji/png/128", 
+        "raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis");
+        
     return {
         redirectUrl: newUrl
     };
 }
 
+// Intercept all requests to Google CDN of type 'image' before they are made
 browser.webRequest.onBeforeRequest.addListener(
     redirect,
     {urls: [pattern], types:["image"]},
