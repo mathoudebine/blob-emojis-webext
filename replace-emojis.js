123da377fa7484da283b13cdab3a374bde558476
mathoudebine <mat.houdebine@wanadoo.fr>
2018-08-21 10:21:31 +0200

    Replace request URL for both 32px and 128px emojis

--- replace-emojis.js
+++ replace-emojis.js
@@ -2,8 +2,10 @@ var pattern = "*://ssl.gstatic.com/dynamite/emoji/*";
 
 function redirect(requestDetails) {
     // Replace origin URL with archived Google Emoji equivalent from noto-emoji repository
+    newUrl = requestDetails.url.replace("ssl.gstatic.com/dynamite/emoji/png/32","raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis");
+    newUrl = newUrl.replace("ssl.gstatic.com/dynamite/emoji/png/128", "raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis");
     return {
-        redirectUrl: requestDetails.url.replace("ssl.gstatic.com/dynamite/emoji/png/32", "raw.githubusercontent.com/mathoudebine/potatoes-emoji-webext/master/emojis")
+        redirectUrl: newUrl
     };
 }
 
